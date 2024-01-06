6f67b4ae7aacc67ba7177305c4acce057814a476
Emirhan Karahan <emirhan.karahan@asiselektronik.com.tr>
2024-01-06 23:55:43 +0300

    Initial Commit

---
+++ mulTiple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mulTiple
+//
+//  Created by Emirhan Karahan on 6.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
